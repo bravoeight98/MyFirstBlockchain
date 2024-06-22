7c7a0f14cc871d1d90917c664affb13d509eef77
Azfar Shams <azfarshams1998@gmail.com>
2024-06-22 14:53:19 +0600

    Update MFB.py

--- MFB.py
+++ MFB.py
@@ -1,2 +1,7 @@
 import hashlib
 
+class MFB_Block:
+
+    def __init__ (self, previous_block_hash, transaction_list):
+        self.previous_block_hash = previous_block_hash
+        self.transaction_list = transaction_list
