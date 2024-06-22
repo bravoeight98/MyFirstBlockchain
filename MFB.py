1e0bbbb2b91335dc5dc05abde568c02ec63cb3b3
Azfar Shams <azfarshams1998@gmail.com>
2024-06-22 15:01:17 +0600

    Update MFB.py

--- MFB.py
+++ MFB.py
@@ -9,3 +9,10 @@ class MFB_Block:
         self.block_data = "-".join(transaction_list) + "-" + previous_block_hash
         self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
 
+
+t1 = "Anna sends 2 MC to Mike" 
+t2 = "Bob sends 4.1 MC to Mike" 
+t3 = "Mike sends 3.2 MC to Bob" 
+t4 = "Daniel sends 0.3 MC to Anna" 
+t5 = "Mike sends 1 MC to Charlie" 
+t6 = "Mike sends 5.4 MC to Daniel"
